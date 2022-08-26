9cdb6ea3dbda556519c224f0893c9d8202e6e412
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-08-26 16:48:10 -0700

    Add dev dependency: cross-env

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3010;
+const PORT = process.env.PORT || 3010;
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
